dbea06a9881df15fe1629c0d8876b936ea2ff10c
bertoaldo <luke.ocallaghan1@gmail.com>
2019-03-17 14:49:22 +1100

    page the posts

--- client.c
+++ client.c
@@ -63,6 +63,9 @@ int main()
 	int rowcount = max_length * 10;
 	int thread_pos = 0;
 	int attr = 0;
+	int depth = 0;
+	int *thread_loc = malloc(sizeof(int) * max_length);
+	int x = 0, y = 0;
 
 	// create pad
 	pad = newpad (rowcount+2, mcol);
@@ -74,16 +77,21 @@ int main()
 	wrefresh(pad);
 
 	for (int i = 0; i < max_length; i++) {
-		attr = (i == thread_pos) ? A_REVERSE | A_BLINK : A_REVERSE;
+		attr = (i == thread_pos) ? A_REVERSE | A_BLINK | A_BOLD : A_REVERSE;
+		getyx(pad, y, x);
+		thread_loc[i] = y;
 		wattron(pad, attr);
-		wprintw(pad, "[ id: %s ]\n", ht_search(ht[i], "id"));
+		if(i == thread_pos) {
+			wprintw(pad, ">[ id: %s ]\n", ht_search(ht[i], "id"));
+		} else {
+			wprintw(pad, "[ id: %s ]\n", ht_search(ht[i], "id"));
+		}
 		wattroff(pad, attr);
 		wprintw(pad, "%s\n@ %s\n(replies: %s)\n\n",  ht_search(ht[i], "content"),  ht_search(ht[i], "created"),  ht_search(ht[i], "replies"));
 	}
 
 	// Show content of pad
-	int mypadpos = 0;
-	prefresh(pad, mypadpos, 0, 1, 0, mrow-1, mcol);
+	prefresh(pad, 0, 0, 1, 0, mrow-1, mcol);
 
 	// wait for exit key
 	int ch;
@@ -91,39 +99,41 @@ int main()
 	while((ch = wgetch(pad)) != 'q') {
 		switch (ch) {
 			case KEY_UP:
-				if (mypadpos > 0) {
-					mypadpos--;
-					thread_pos--;
-				}
+					thread_pos -= (thread_pos == 0) ? 0 : 1;
 			break;
 			case KEY_DOWN:
-				if (mypadpos < rowcount+1) {
-					mypadpos++;
-					thread_pos++;
-				}
+					thread_pos += (thread_pos == max_length - 1) ? 0 : 1;
 			break;
 			case KEY_RIGHT:
 				//
-				max_length = loadPost(ht, max_length, thread_pos);
-				thread_pos = 0;
-				mypadpos = 0;
+				if (depth == 0) {
+					max_length = loadPost(ht, max_length, thread_pos);
+					thread_pos = 0;
+					depth++;
+				}
 			break;
 			case KEY_LEFT:
 				//
-				//max_length = back(ht, max_length, thread_pos);
-				//thread_pos = 0;
-				//mypadpos = 0;
+				if (depth == 1) {
+					//max_length = back(ht, max_length);
+					thread_pos = 0;
+					depth--;
+				}
 			break;
 		}
 		wclear(pad);
 		for (int i = 0; i < max_length; i++) {
-			attr = (i == thread_pos) ? A_REVERSE | A_BLINK : A_REVERSE;
+			attr = (i == thread_pos) ? A_REVERSE | A_BLINK | A_BOLD : A_REVERSE;
 			wattron(pad, attr);
-			wprintw(pad, "[ id: %s ]\n", ht_search(ht[i], "id"));
+			if(i == thread_pos) {
+				wprintw(pad, ">[ id: %s ]\n", ht_search(ht[i], "id"));
+			} else {
+				wprintw(pad, "[ id: %s ]\n", ht_search(ht[i], "id"));
+			}
 			wattroff(pad, attr);
 			wprintw(pad, "%s\n@ %s\n(replies: %s)\n\n",  ht_search(ht[i], "content"),  ht_search(ht[i], "created"),  ht_search(ht[i], "replies"));
 		}
-		prefresh(pad, mypadpos, 0, 1, 0, mrow-1, mcol);
+		prefresh(pad, thread_loc[thread_pos] - 2, 0, 1, 0, mrow-1, mcol);
 	}
 
 	// remove window
